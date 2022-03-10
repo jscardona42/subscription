9354a97d5593461fd808ac1e602802aeac3ce198
Mario Cristancho <mcristancho@tiresia.co>
2022-03-10 08:14:58 -0500

    CME-39 configuracion para SSL Directorio para agregar certificado y llave

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,40 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GqlAuthGuard } from './modules/auth/guard/authguard.guard';
+import { NestApplicationOptions, Logger } from '@nestjs/common';
+import { join } from 'path';
+import { readFileSync } from 'fs';
 
-
+const options: NestApplicationOptions = {};
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalGuards(new GqlAuthGuard());  
-  await app.listen(process.env.MS_PORT);
+  const logger = new Logger('MAIAPUBSUB');
+
+  logger.log('USE_SSL:' + process.env.USE_SSL);
+  const isProduction: boolean = process.env.USE_SSL === 'true';
+
+  const port = process.env.MS_PORT || 3005;
+  let http = 'HTTP';
+
+  if (isProduction) {
+    const secretsDir = join(__dirname, '..', 'secrets');
+    logger.log('SSL certificate dir:' + secretsDir);
+    try {
+      const httpsOptions = {
+        key: readFileSync(`${secretsDir}/key.pem`),
+        cert: readFileSync(`${secretsDir}/cert.pem`),
+      };
+      options.httpsOptions = httpsOptions;
+      http = 'HTTPS';
+    } catch (error) {
+      logger.log(
+        'No SSL cert found, starting server without SSL. Error:' + error,
+      );
+    }
+  }
+  const app = await NestFactory.create(AppModule, options);
+  app.useGlobalGuards(new GqlAuthGuard());
+  await app
+    .listen(port)
+    .then(() => logger.log(`${http} Server running on port ${port}`));
 }
 bootstrap();
