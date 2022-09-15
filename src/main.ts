97a286ba6d1b84ba031466b789eaadd6b9ea0b9d
jscardonag <jcardona@tiresia.co>
2022-09-14 20:15:28 -0500

    MCE-163 Modificación Https

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GqlAuthGuard } from './modules/auth/guard/authguard.guard';
-import { NestApplicationOptions, ValidationPipe } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import * as fs from 'fs';
 let https = require('https');
+let http = require('http');
 const express = require("express");
 
-const options: NestApplicationOptions = {};
 async function bootstrap() {
-
   const options: any = {};
+  const server = express();
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(server),
+  );
 
-  // validamos si los archivos existen
-  if (fs.existsSync(process.env.CERTIFICATE_SSL) && fs.existsSync(process.env.KEY_SSL)) {
+  app.useGlobalGuards(new GqlAuthGuard());
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+
+  if (process.env.ENABLE_SSL === "true" && fs.existsSync(process.env.CERTIFICATE_SSL) && fs.existsSync(process.env.KEY_SSL)) {
     options.httpsOptions = {
       cert: fs.readFileSync(process.env.CERTIFICATE_SSL),
       key: fs.readFileSync(process.env.KEY_SSL),
       requestCert: false,
       rejectUnauthorized: false,
     }
+    await app.init();
+    https.createServer(options.httpsOptions, server).listen(process.env.PORT || 3005);
+  } else {
+    await app.init();
+    http.createServer(server).listen(process.env.PORT || 3005);
   }
-
-  const server = express();
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(server),
-  );
-  app.useGlobalGuards(new GqlAuthGuard());
-  app.useGlobalPipes(new ValidationPipe());
-  await app.init();
-
-  https.createServer(options.httpsOptions, server).listen(process.env.PORT || 3005);
-
 }
 bootstrap();
