4570a9b48e7cc1db438720e4408a39fcb6793bd7
jscardonag <jcardona@tiresia.co>
2022-09-29 12:28:12 -0500

    MCE-163 Modificación protocolo Https

--- src/main.ts
+++ src/main.ts
@@ -2,36 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GqlAuthGuard } from './modules/auth/guard/authguard.guard';
 import { ValidationPipe } from '@nestjs/common';
-import { ExpressAdapter } from '@nestjs/platform-express';
 import * as fs from 'fs';
-let https = require('https');
-let http = require('http');
-const express = require("express");
 
 async function bootstrap() {
-  const options: any = {};
-  const server = express();
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(server),
-  );
-
-  app.useGlobalGuards(new GqlAuthGuard());
-  app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
+  let options: any = {};
 
   if (process.env.ENABLE_SSL === "true" && fs.existsSync(process.env.CERTIFICATE_SSL) && fs.existsSync(process.env.KEY_SSL)) {
     options.httpsOptions = {
       cert: fs.readFileSync(process.env.CERTIFICATE_SSL),
-      key: fs.readFileSync(process.env.KEY_SSL),
-      requestCert: false,
-      rejectUnauthorized: false,
+      key: fs.readFileSync(process.env.KEY_SSL)
     }
-    await app.init();
-    https.createServer(options.httpsOptions, server).listen(process.env.PORT || 3005);
-  } else {
-    await app.init();
-    http.createServer(server).listen(process.env.PORT || 3005);
   }
+
+  const app = await NestFactory.create(AppModule, options);
+  app.useGlobalGuards(new GqlAuthGuard());
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  await app.listen(process.env.PORT || 3005)
 }
 bootstrap();
